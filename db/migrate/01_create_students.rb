abc7a62dda3ef43175a6a7b16ca7d873ba7431e5
Joseph Kao <josephtkao@gmail.com>
2020-06-22 23:57:01 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,14 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+  )
+
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name STRING
+  )
+  SQL
+  
 end
