4e7dd8b7abedca77a941c910facddf876bb8c61b
Joseph Kao <josephtkao@gmail.com>
2020-06-23 00:11:19 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,5 @@
 class AddGradeAndBirthdateToStudents <  ActiveRecord::Migration[5.1]
   def change
-
+    add_column :students, :grade, :INTEGER
+    add_column :students, :birthdate, :STRING
   end
