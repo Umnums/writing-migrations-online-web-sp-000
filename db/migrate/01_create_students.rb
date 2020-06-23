1a1d3df39ed048069127d91d4d358112ced2a71f
Joseph Kao <josephtkao@gmail.com>
2020-06-23 00:07:44 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,15 +1,11 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-  )
-
-  sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS students(
-    id INTEGER PRIMARY KEY,
-    name STRING
-  )
-  SQL
-  ActiveRecord::Base.connection.execute(sql)
+  def change
+      create_table :artists do |t|
+        t.string :name
+        t.string :genre
+        t.integer :age
+        t.string :hometown
+      end
+    end
 
 end
